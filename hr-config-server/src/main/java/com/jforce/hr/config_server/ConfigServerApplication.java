c5be65e5a52b6fd4e3a77a5c778e8c37582b5649
fdundar <fatih.dundar@medisasaglik.com.tr>
2024-07-22 19:53:12 +0300

    first commit

--- hr-config-server/src/main/java/com/jforce/hr/config_server/ConfigServerApplication.java
+++ hr-config-server/src/main/java/com/jforce/hr/config_server/ConfigServerApplication.java
@@ -1,10 +1,14 @@
 package com.jforce.hr.config_server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @EnableConfigServer
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ConfigServerApplication {
 
